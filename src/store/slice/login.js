af43b9dc083164b85d3d51b7eb1f15b1cfff34aa
roywong013 <74874696+roywong013@users.noreply.github.com>
2022-10-25 11:43:40 +0800

    Adding Local Storage

--- src/store/slice/login.js
+++ src/store/slice/login.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { isLogin: false, idToken: "" };
+const initialToken = localStorage.getItem("token");
+
+const userIsLogin = !!initialToken;
+
+const initialState = { isLogin: userIsLogin, idToken: initialToken };
 
 const slice = createSlice({
   name: "login",
@@ -9,11 +13,12 @@ const slice = createSlice({
     login(state, action) {
       state.isLogin = true;
       state.idToken = action.payload;
+      localStorage.setItem("token", action.payload);
     },
     logout(state) {
       state.isLogin = false;
       state.idToken = "";
-      console.log(state.idToken);
+      localStorage.removeItem("token");
     }
   }
 });
